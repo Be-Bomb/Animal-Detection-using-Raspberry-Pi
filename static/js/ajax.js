370a8ad759f301808c91c710bdcacac2f69deeec
thispath98 <l0641@naver.com>
2022-03-20 00:06:47 +0900

    Chore: 파일 위치 수정
    css, images, js, videos 폴더를 static 폴더의 하위 폴더로 옮김.

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -5,13 +5,28 @@ $(document).ready(function () {
             url: "/data_chart",
             dataType: "json",
             success: function (result) {
-                $("#text").html(JSON.stringify(result, null, 4));
+                $("#dataChart").html(JSON.stringify(result, null, 4));
             },
+            error: function() {
+                console.log("data chart fail")
+            }
         });
     }, 1000 * 0.5);
-
+    
     $("#storage").click(function () {
         $(".modal").fadeIn();
+        
+        $.ajax({
+            type: "get",
+            url: "/get_images",
+            success: function () {
+                // 모달 창을 새로고침하여 데이터를 가져온다.
+                $(".modal_content").load(window.location.href + " .modal_content")
+            },
+            error: function() {
+                console.log("get images fail")
+            }
+        });
     });
 
     $(".modal").click(function () {
