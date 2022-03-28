29479787ae32f9c6594a3a205f99308c96716f9d
thispath98 <l0641@naver.com>
2022-03-28 18:05:48 +0900

    Chore: navbar 구현
    navbar를 구현하였습니다. 또한 formatting style을 일부 변경하였고, Google Font API를 추가하였습니다.
    
    Storage 버튼과 Data Chart 버튼의 id도 일부 바꾸었습니다.

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -7,25 +7,25 @@ $(document).ready(function () {
             success: function (result) {
                 $("#dataChart").html(JSON.stringify(result, null, 4));
             },
-            error: function() {
-                console.log("data chart fail")
-            }
+            error: function () {
+                console.log("data chart fail");
+            },
         });
     }, 1000 * 0.5);
-    
-    $("#storage").click(function () {
+
+    $("#storageButton").click(function () {
         $(".modal").fadeIn();
-        
+
         $.ajax({
             type: "get",
             url: "/get_images",
             success: function () {
                 // 모달 창을 새로고침하여 데이터를 가져온다.
-                $(".modal_content").load(window.location.href + " .modal_content")
+                $(".modal_content").load(window.location.href + " .modal_content");
             },
-            error: function() {
-                console.log("get images fail")
-            }
+            error: function () {
+                console.log("get images fail");
+            },
         });
     });
 
