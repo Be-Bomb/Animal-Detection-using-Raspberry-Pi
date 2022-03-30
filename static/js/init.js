0860d19678ec5f0652cf2b53750241dbeb391aea
thispath98 <l0641@naver.com>
2022-03-30 18:29:09 +0900

    Chore: .js 파일 통합
    ajax.js와 init.js를 통합하여 코드 가독성을 높였습니다.

--- static/js/init.js
+++ static/js/init.js
@@ -1,26 +1,65 @@
-// record start & set time in sessionStorage
-function record_start() {
+$(function () {
+    // 현재 시간 출력
+    setInterval(function () {
+        const recordingTime = new Date();
+        $("#presentTime").text(recordingTime.toLocaleString());
+
+        // 녹화 시간 출력
+        const startTimeOfRecord = localStorage.getItem("startTimeOfRecord");
+        if (startTimeOfRecord) {
+            const parsedRecordTime = parseInt(startTimeOfRecord);
+
+            const hours = parseInt((recordingTime - parsedRecordTime) / 1000 / 60 / 60);
+            const minutes = parseInt((recordingTime - parsedRecordTime) / 1000 / 60);
+            const seconds = parseInt((recordingTime - parsedRecordTime) / 1000);
+            const text = `${hours > 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+            $("#recordingClock").text(text);
+        }
+
+        $.ajax({
+            type: "get",
+            url: "/data_chart",
+            dataType: "json",
+            success: function (result) {
+                $("#dataChart").html(JSON.stringify(result, null, 4));
+            },
+            error: function () {
+                console.log("data chart fail");
+            },
+        });
+    }, 1000);
+});
+
+// 녹화 시작 버튼
+$("#recordStart").click(function () {
     const startTimeOfRecord = Date.now();
-    sessionStorage.setItem("startTimeOfRecord", startTimeOfRecord);
-}
+    localStorage.setItem("startTimeOfRecord", startTimeOfRecord);
+});
 
-// show recording time when on-air per 1 minutes
-setInterval(function () {
-    const Time = document.getElementById("presentTime");
-    const recordingTime = new Date();
-    Time.innerText = recordingTime.toLocaleString();
+// 녹화 종료 버튼
+$("#recordStop").click(function () {
+    localStorage.removeItem(startTimeOfRecord);
+});
 
-    const Target = document.getElementById("recordingClock");
-    const startTimeOfRecord = parseInt(sessionStorage.getItem("startTimeOfRecord"));
+// 모달 창 열기
+$("#storageButton").click(function () {
+    $(".modal").fadeIn();
 
-    if (Target){
+    $.ajax({
+        type: "get",
+        url: "/get_images",
+        success: function () {
+            // 모달 창을 새로고침하여 데이터를 가져온다.
+            $(".modal_content").load(window.location.href + " .modal_content");
+        },
+        error: function () {
+            console.log("get images fail");
+        },
+    });
+});
 
-        const hours = parseInt((recordingTime - startTimeOfRecord) / 1000 / 60 / 60);
-        const minutes = parseInt((recordingTime - startTimeOfRecord) / 1000 / 60);
-        const seconds = parseInt((recordingTime - startTimeOfRecord) / 1000);
-        
-        Target.innerText = `${hours > 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${
-            seconds < 10 ? `0${seconds}` : seconds
-        }`;
-    }
-}, 1000 * 0.5);
+// 모달 창 닫기
+$(".modal").click(function () {
+    $(".modal").fadeOut();
+});
